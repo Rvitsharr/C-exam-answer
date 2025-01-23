8c832d315358f4805bf318f16af75da681aa18e7
Win Thiha <63143936+Rvitsharr@users.noreply.github.com>
2025-01-23 13:41:41 +0630

    Update quest_03.cpp

--- quest_03.cpp
+++ quest_03.cpp
@@ -2,14 +2,22 @@
 #include <array>
 #include <algorithm>
 
+std::pair<int, int> getMinMax(const std::array<int, 7>& exam_arr)
+{
+    auto [min, max] = std::minmax_element(exam_arr.begin(), exam_arr.end());
+
+    return {*min, *max};
+
+}
+
 int main()
 {
     std::array<int, 7> exam_arr = {4,6,39,73, 2,5 ,7};
 
-    auto [min, max]  = std::minmax_element(exam_arr.begin(), exam_arr.end());
+    auto [min, max] = getMinMax(exam_arr);
 
-    std::cout << "Min: " << *min << std::endl;
-    std::cout << "Max: " << *max << std::endl;
+    std::cout << "Min: " << min << std::endl;
+    std::cout << "Max: " << max << std::endl;
     
     return 0;
 }
